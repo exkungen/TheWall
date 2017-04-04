48dca2d7849ef080a812e87f1c7d1207c701517f
exkungen <23136@ma-web.nl>
2017-04-04 12:27:44 +0200

    MAster files
    voor het project

---
+++ login.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+    <title>The Wall - Registratie</title>
+</head>
+<body>
+<div id="wrapper">
+    <nav>
+        <ul>
+            <li><a  href="index.html">Home</a> </li>
+            <li> <a href="registratie.php">Registreer</a> </li>
+
+            <li><a  href="upload.php">Upload</a> </li>
+            <li><a class="active" href="login.php">Login</a> </li>
+        </ul>
+
+    </nav><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <form>
+        <div id="naam">
+        <label for="fname">Naam:</label><br>
+        <input type="text" placeholder="Naam" id="fname" name="fname">
+        </div><br>
+        <label for="lname">Wachtwoord:</label><br>
+        <input type="text" id="lname" placeholder="Wachtwoord" name="lname"><br>
+
+        <input type="submit" value="Login" name="login">
+    </form>
+
+    <div class="push"></div>
+</div>
+<footer class="footer">
+
+    <h1>The Wall - Melissa en Menouer</h1>
+</footer>
+
+</body>
+</html>
